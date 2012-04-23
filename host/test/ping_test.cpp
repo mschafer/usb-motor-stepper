bb9dea7aae3a623530f92b4e02c925509083f536
mschafer <mschafer@alum.mit.edu>
2012-04-23 16:03:45 -0400

    tests depend on lib

---
+++ host/test/ping_test.cpp
@@ -0,0 +1,10 @@
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+#include "ums.h"
+
+
+BOOST_AUTO_TEST_CASE( ping_test )
+{
+    BOOST_CHECK( 2+2 == 4 );
+}
